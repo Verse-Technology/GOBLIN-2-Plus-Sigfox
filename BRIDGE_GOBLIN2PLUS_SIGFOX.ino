170a6187413e4be56daac53ceb854d19ddd2cffc
Julio Zamudio <waterhouse.julio@gmail.com>
2018-01-23 11:15:49 -0600

    Bridge_GOBLIN2PLUS_SIGFOX
    WITH THIS PROGRAM YOU CAN HAVE COMMUNICATION BETWEEN MODULE WISOL RCZ2 (SFM10R2) AND YOUR SAMD21.

---
+++ BRIDGE_GOBLIN2PLUS_SIGFOX.ino
@@ -0,0 +1,40 @@
+/*     BRIDGE_GOBLIN2PLUS SIGFOX.      
+       
+  WITH THIS PROGRAM YOU CAN HAVE COMMUNICATION BETWEEN MODULE WISOL RCZ2 (SFM10R2) AND YOUR SAMD21.
+  
+  Check more documentation and examples in https://verse-technology.com
+ 
+  modified 2 December 2017.
+  by Rafa Cortés.
+  -GOBLIN 2 Plus TEAM. VERSE Technology.
+  
+*/
+
+void setup() {
+  SerialUSB.begin(9600);    
+  Serial.begin(9600);
+  while (!SerialUSB);
+}
+
+void loop() {
+  
+  if (Serial.available()) {   
+      String toSend = ""; 
+      while (Serial.available())  {   
+      toSend += (char)Serial.read();
+                                      }
+      SerialUSB.print(toSend);
+                                    } 
+
+  if (SerialUSB.available()) {   
+      String toSend = ""; 
+      while (SerialUSB.available())  {   
+      toSend += (char)SerialUSB.read(); 
+      }
+  
+     Serial.print(toSend);
+  }
+
+  }
+
+
